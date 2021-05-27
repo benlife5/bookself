ef99c2b07d7987df8ae75c49181ebebe721be167
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-05-27 15:05:59 -0400

    /library added

--- server/server.js
+++ server/server.js
@@ -9,6 +9,17 @@ app.get("/", (req, res) => {
   res.send("Bookshelf Server Active");
 });
 
+app.get("/library", async (req, res) => {
+  const { user } = req.query;
+  const libraryRef = db.collection("users").doc(user).collection("library");
+  const library = await libraryRef.get();
+  let result = {};
+  library.forEach((doc) => {
+    result[doc.id] = doc.data();
+  });
+  res.send(result);
+});
+
 app.listen(PORT, () => {
   console.log("Bookshelf Server Running on port " + PORT);
 });
