449a8b978b8ece12b05dd6d513edef85de975180
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-05-28 09:48:41 -0400

    add /add POST method

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const PORT = 8080;
 
 app.get("/", (req, res) => {
@@ -43,9 +44,36 @@ app.get("/search", async (req, res) => {
   }
 });
 
-app.post("/add", (req, res) => {
-  
-})
+app.post("/add", async (req, res) => {
+  const { userId, bookId } = req.body;
+  console.log(userId, bookId);
+  const dbLocation = db
+    .collection("users")
+    .doc(userId)
+    .collection("library")
+    .doc(bookId);
+
+  const currentDoc = await dbLocation.get();
+  if (currentDoc.exists)
+    res.send({ added: false, message: "Book already in library" });
+
+  try {
+    const book = await axios.get(
+      "https://www.googleapis.com/books/v1/volumes/" + bookId
+    );
+    console.log(book.data);
+    await dbLocation.set(book.data);
+
+    res.send({ added: true, message: "Successfully added to library" });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      added: false,
+      message: "Error adding to library",
+      error: JSON.stringify(error),
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Bookshelf Server Running on port " + PORT);
